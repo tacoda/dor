d500d28ecc5ae5802e458ed2a193bb4bc148a9c2
Ian Johnson <ijohnson@covermymeds.com>
2019-12-22 11:53:16 -0500

    Change parameter order and add links to README

---
+++ primal/README.md
@@ -0,0 +1,6 @@
+# Primal
+
+[Divisor function](https://en.wikipedia.org/wiki/Divisor_function)
+[Miller-Rabin primality test](https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test)
+[GMP Algorithms Page](https://gmplib.org/manual/Algorithms.html#Algorithms)
+[Consecutive Prime Sum](https://projecteuler.net/problem=50)
--- primal/src/main.rs
+++ primal/src/main.rs
@@ -2,16 +2,16 @@ extern crate primal;
 
 use primal::Sieve;
 
-fn determine_primality(sieve: &Sieve, num: usize) -> bool {
+fn determine_primality(num: usize, sieve: &Sieve) -> bool {
     sieve.is_prime(num)
 }
 
-fn find_nth_prime(sieve: &Sieve, num: usize) -> Option<usize> {
+fn find_nth_prime(num: usize, sieve: &Sieve) -> Option<usize> {
     sieve.primes_from(0).nth(num - 1)
 }
 
-fn num_divisors(n: usize, primes: &Sieve) -> Option<usize> {
-    match primes.factor(n) {
+fn num_divisors(num: usize, primes: &Sieve) -> Option<usize> {
+    match primes.factor(num) {
         Ok(factors) => Some(factors.into_iter().fold(1, |acc, (_, x)| acc * (x + 1))),
         Err(_) => None,
     }
@@ -21,19 +21,19 @@ fn main() {
     let sieve = Sieve::new(10000);
     let suspect = 5273;
     // println!("{} is prime: {}", suspect, sieve.is_prime(suspect));
-    println!("{} is prime: {}", suspect, determine_primality(&sieve, suspect));
+    println!("{} is prime: {}", suspect, determine_primality(suspect, &sieve));
 
 
     let not_a_prime = 1024;
     // println!("{} is prime: {}", not_a_prime, sieve.is_prime(not_a_prime));
-    println!("{} is prime: {}", not_a_prime, determine_primality(&sieve, not_a_prime));
+    println!("{} is prime: {}", not_a_prime, determine_primality(not_a_prime, &sieve));
 
     let n = 1000;
     // match sieve.primes_from(0).nth(n - 1) {
     //     Some(number) => println!("{}th prime is {}", n, number),
     //     None => println!("I don't know anything about the {}th prime.", n),
     // }
-    match find_nth_prime(&sieve, n) {
+    match find_nth_prime(n, &sieve) {
         Some(number) => println!("{}th prime is {}", n, number),
         None => println!("I don't know anything about the {}th prime.", n),
     }
