b357a7ff1eb5733784e130110eeed8727a5b1633
Ian Johnson <ijohnson@covermymeds.com>
2020-01-04 20:15:50 -0500

    Rename exercises

--- hyper_learn/src/main.rs
+++ hyper_learn/src/main.rs
@@ -3,12 +3,24 @@ extern crate tokio;
 
 use hyper_learn;
 
+// ***Server***
+
+// Getting started:
+// #[tokio::main]
+// async fn main() {
+//     hyper_learn::server::start::main().await
+// }
+
+// Echo:
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    hyper_learn::server::echo::main().await
+}
+
+// ***Client***
+
+// Getting started:
 // #[tokio::main]
 // async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 //     hyper_learn::client::start::main().await
 // }
-
-#[tokio::main]
-async fn main() {
-    hyper_learn::server::start::main().await
-}
